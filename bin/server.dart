86b4711f4c4c54872980d7b926a3e0b827f69a8e
Robert Åkerblom-Andersson <Scorpiion@users.noreply.github.com>
2017-07-28 14:10:03 +0200

    Update server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -2,7 +2,7 @@ import 'package:redstone/redstone.dart' as app;
 
 @app.Route("/")
 @app.Route("/dart")
-helloWorld() => "Test 2";
+helloWorld() => "Test 3";
 
 void main() {
   app.setupConsoleLog();
