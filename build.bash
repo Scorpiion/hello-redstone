2151aeec00f84a82354be21d38ecfd49cebb7c2c
Robert Åkerblom-Andersson <robert@sourcevoid.com>
2017-07-30 21:31:06 +0200

    Save

--- build.bash
+++ build.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Sleeping 240"
+sleep 240
+
+echo "Sleeping 10"
+sleep 10
+
 echo "mount"
 mount
 
@@ -25,4 +31,26 @@ echo "pub get"
 pub get
 
 
+exit 0 
 
+-----------------
+
+TODO: 
+
+- Remove old git package rewrites in bootstrap.bash
+- Run a build pipeline with sleep 1000 and test manually...
+
+
+ERRORS from deploy/build:
+
+
+    fatal: unable to read config file '/home/cuser/cache/.gitconfig': No such file or directory
+    Cloning into 'sourcevoid_client'...
+    fatal: could not read Username for 'https://github.com': No such device or address
+
+    
+    Cloning into 'hello-redstone'...
+    fatal: '/home/cuser/pkgs/github/Scorpiion/hello-redstone.git' does not appear to be a git repository
+
+    
+    
